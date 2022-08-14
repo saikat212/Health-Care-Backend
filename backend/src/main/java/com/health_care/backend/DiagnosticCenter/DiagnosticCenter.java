ef573f0854393e6f7b91fde4d97862f054ba58be
saikat <saikat212saikat@gmail.com>
2022-08-14 15:13:06 +0600

    DCAdmin SignUp Successfully worked

--- backend/src/main/java/com/health_care/backend/DiagnosticCenter/DiagnosticCenter.java
+++ backend/src/main/java/com/health_care/backend/DiagnosticCenter/DiagnosticCenter.java
@@ -1,11 +1,10 @@
 package com.health_care.backend.DiagnosticCenter;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
+import com.health_care.backend.Person.Person;
+import com.health_care.backend.TakerSpeciality.TakerSpeciality;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,11 +17,15 @@ import lombok.NoArgsConstructor;
 public class DiagnosticCenter {
     @Id
     @GeneratedValue
-
     private Integer id;
+
     private String name;
     private String location;
     private String isOfferOnsiteTest; // 1 = offer -------- 0 = don't offer
     private String registrationNum;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "admin_id", referencedColumnName = "id")
+    private Person person;
     
 }
