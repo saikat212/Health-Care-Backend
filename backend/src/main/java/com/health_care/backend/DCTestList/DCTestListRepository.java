0d41cd1e2901bf276f70bb3a58fcb9e7f403ee58
saikat <saikat212saikat@gmail.com>
2022-08-16 21:40:08 +0600

    Possible Full DC Find and Appoinment Book Done

--- backend/src/main/java/com/health_care/backend/DCTestList/DCTestListRepository.java
+++ backend/src/main/java/com/health_care/backend/DCTestList/DCTestListRepository.java
@@ -1,7 +1,23 @@
 package com.health_care.backend.DCTestList;
 
+import com.health_care.backend.Doctor.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DCTestListRepository extends JpaRepository<DCTestList, Integer> {
+
+//
+//    @Query("SELECT D FROM DCTestList D JOIN Person P "+
+//            "ON(P.id = D.id)"+
+//            "WHERE P.firstName LIKE CONCAT('%',:query,'%') "+
+//            "OR P.lastName LIKE CONCAT('%',:query,'%')"
+//    )
+    @Query("SELECT D FROM DCTestList D JOIN Test T "+
+            "ON(T.id = D.id)"+
+            "WHERE T.name LIKE CONCAT('%',:testname,'%') "
+    )
+    List<DCTestList> findDCByTestname(String testname);
 
 }
--- backend/src/main/java/com/health_care/backend/DCTestList/DCTestListService.java
+++ backend/src/main/java/com/health_care/backend/DCTestList/DCTestListService.java
@@ -2,6 +2,7 @@ package com.health_care.backend.DCTestList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -14,4 +15,6 @@ public class DCTestListService {
         return repository.save(dc_Test);
     }
     public List<DCTestList> getAllDCTestList() { return  repository.findAll();}
+
+    public List<DCTestList> getDCByTestname(@PathVariable String testname) { return  repository.findDCByTestname(testname);}
 }
