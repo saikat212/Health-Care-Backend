6754d3f3b4f49d9dcb1d08a3eac94ab3553f8249
Md. Sakibur Reza <sakiburrezajony@gmail.com>
2022-08-28 23:30:23 +0600

    Final commit

--- backend/src/main/java/com/health_care/backend/Doctor/DoctorController.java
+++ backend/src/main/java/com/health_care/backend/Doctor/DoctorController.java
@@ -1,7 +1,5 @@
 package com.health_care.backend.Doctor;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
